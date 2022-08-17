8a8fa2f01cbc19170dcb4921b6929c0e8f3045ae
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-08-17 08:25:22 -0500

    Finish send birthday email

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-##################### Extra Hard Starting Project ######################
+from datetime import datetime
+import smtplib
+import pandas
+import random
 
-# 1. Update the birthdays.csv
+USR = "alucardluis@hotmail.com"
+PWD = "password"
 
-# 2. Check if today matches a birthday in the birthdays.csv
+today = (datetime.now().month, datetime.now().day)
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+data = pandas.read_csv("birthdays.csv")
 
-# 4. Send the letter generated in step 3 to that person's email address.
+birthday_dict = {
+    (data_row.get("month"), data_row.get("day")): data_row for (index, data_row) in data.iterrows()
+}
 
+if today in birthday_dict:
+    birthday_person = birthday_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path, 'r') as file:
+        content = file.read()
+        new_content = content.replace("[NAME]", birthday_person.get('name'))
 
-
-
+    with smtplib.SMTP(host="smtp.office365.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=USR, password=PWD)
+        connection.sendmail(
+            from_addr=USR,
+            to_addrs=birthday_person.get('email'),
+            msg=f"Subject: Happy Birthday!\n\n{new_content}"
+        )
